fbd64d639ea94a3073856c8de9ae538710802f59
Stefan Adams <s.adams@hotmail.be>
2018-07-04 11:32:13 +0200

    Initial commit

---
+++ Example/Paginator/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Paginator
+//
+//  Created by AdamsDevelopment on 07/04/2018.
+//  Copyright (c) 2018 AdamsDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
